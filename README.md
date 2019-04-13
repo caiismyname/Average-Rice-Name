bacb9d4b3659f295bdb1423c58d32d5db16c3ef1
caiismyname <davidcai2012@gmail.com>
2019-04-13 16:00:53 -0500

    smoothing computation added

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Average name of Rice UG
 
-## The average name (2019) is `aani san`. With the `-l` option it's `aaniennneeret sannenaeiairrezeiais`.
+- The average name (2019) is `Aani San`. 
+- With the `-l` option it's `Aaniennneeret Sannenaeiairrezeiais`.
+- Using the smoothing option (`-s`), the average name is `Aaaiie Saaaenn`
+- Using smoothing with true mean (`-s -a`), the average name is `Ijijkjj Kkjkkk`.
 
 This script computes the average name of current Rice undergrads (taken from `search.rice.edu`). It defines the average name as the name comprised of the most common letter for each position in the name. So really it's more of a "mode" sort of calculation. 
 
-To reconcile long/short names, whitespace is padded to the end of short names up until the length of the longest name. Use the `-l` flag to compute the average name without this padding (longer names will have more weight over the latter portions of the average name).  
+To reconcile long/short names, two options are given:
+- Whitespace is padded to the end of short names up until the length of the longest name. Use the `-l` flag to compute the average name without this padding (longer names will have more weight over the latter portions of the average name).  
+- All names are stretched to the length of the longest name, the average is computed, and a name of the average length is extracted. Use `-s` to use this stretching option. This option also has the choice of defining average as frequency (no additional flag) or true mathematical mean on the ASCII values (`-a` flag). 
 
 This project was inspired by [u/altiarblade's post on reddit](https://www.reddit.com/r/nba/comments/bcm86k/oc_what_was_the_average_name_of_an_nba_player_in/).
--- average_rice_name.py
+++ average_rice_name.py
@@ -2,6 +2,8 @@ import re
 import urllib3
 import operator
 import sys
+import numpy as np
+from collections import defaultdict
 
 # Returns (first, last) tuple of strings
 def cleanName(raw):
@@ -55,9 +57,76 @@ def computeAverage(names, longest, useLongName):
     
     return "".join(mostFreq)
     
+'''
+a -> 0
+z -> 25
+whitespace -> 26
+'''
+def charToNum(c):
+    if c == " ":
+        return 26
+
+    return(ord(c.lower()) - 97)
+
+def numToChar(n):
+    if n == 26:
+        return " "
+    else:
+        return chr(n + 97)
+
+def computeAverage2(names, longest, trueAverage):
+    # Convert all names into "stretched" version
+    longest_f = float(longest)
+    normalizedNames = []
+    for name in names:
+        scalingFactor = longest_f / len(name)
+        scaled = []
+        count = 0
+        for char in name:
+            for _ in range(int(scalingFactor + .5)):
+                if count < longest:
+                    scaled.append(char)
+                    count += 1
+                else:
+                    break
+        while count < longest:
+            scaled.append(name[-1])
+            count += 1
+
+        normalizedNames.append(scaled)
+
+    # All names are now normalized to the length of the maximum name
+
+    # Compute the average name of max size
+    # Method 1: True average
+    averageNameMax = []
+    if trueAverage:
+        numNames = [[charToNum(c) for c in name] for name in normalizedNames]
+        averageNameMax = np.sum(numNames, axis=0) / len(names)
+        averageNameMax = [numToChar(int(x)) for x in averageNameMax]
+    else:
+    # Method 2: Frequency 
+        for i in range(longest):
+            freqs = defaultdict(int)
+            for name in normalizedNames:
+                freqs[name[i]] += 1
+            
+            vals = sorted(freqs.items(), key=operator.itemgetter(1))
+            vals.reverse()
+            averageNameMax.append(vals[0][0])
+        
+    averageNameLen = sum([len(x) for x in names]) / len(names)
+    averageScalingFactor = longest_f / averageNameLen
+    averageName = []
+    idx = 0.0
+    while idx < longest:
+        averageName.append(averageNameMax[min(int(idx), longest - 1)])
+        idx += averageScalingFactor
+
+    return "".join(averageName)
 
 
-def run(useLongName):
+def run(useSmooth, useTrueAverage, useLongName):
     allColleges = ["will+rice", "baker", "lovett", "sid", "hanszen", "wiess", "mcmurtry", "duncan", "brown", "jones", "martel"]
 
     allNames = []
@@ -66,15 +135,21 @@ def run(useLongName):
 
     longestFirst = max([len(x[0]) for x in allNames])
     longestLast = max([len(x[1]) for x in allNames])
+    
+    averageFirst = ""
+    averageLast = ""
 
-    averageFirst = computeAverage([x[0] for x in allNames], longestFirst, useLongName)
-    averageLast = computeAverage([x[1] for x in allNames], longestLast, useLongName)
+    if useSmooth:
+        averageFirst = computeAverage2([x[0] for x in allNames], longestFirst, useTrueAverage)
+        averageLast = computeAverage2([x[1] for x in allNames], longestLast, useTrueAverage)
+    else:
+        averageFirst = computeAverage([x[0] for x in allNames], longestFirst, useLongName)
+        averageLast = computeAverage([x[1] for x in allNames], longestLast, useLongName)
 
     print("It's {} {}".format(averageFirst, averageLast))
 
 
 if len(sys.argv) == 1:
-    run(False)
+    run(False, False, False)
 else:
-    if sys.argv[1] == "-l":
-        run(True)
+    run("-s" in sys.argv, "-a" in sys.argv, "-l" in sys.argv)
